68e1c5507f7d775a8457ebefd62aed08831dc3f0
Lehaneploxo <lehatvel@gmail.com>
2025-12-26 17:45:12 +0200

    Simplify Sanity Studio with dynamic import and minimal config

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,8 +1,28 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '@/sanity.config'
+import dynamic from 'next/dynamic'
+
+const NextStudio = dynamic(() => import('next-sanity/studio').then(mod => mod.NextStudio), {
+  ssr: false,
+  loading: () => <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontSize: '24px'
+  }}>Завантаження Sanity Studio...</div>
+})
 
 export default function StudioPage() {
+  const config = {
+    name: 'default',
+    title: 'Designer Bags UA Admin',
+    projectId: 'alskls9k',
+    dataset: 'production',
+    basePath: '/studio',
+    plugins: [],
+    schema: { types: [] },
+  }
+
   return <NextStudio config={config} />
 }
