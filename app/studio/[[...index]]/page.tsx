e6858b2015a544b336e8b94c05213a572fd5773a
Lehaneploxo <lehatvel@gmail.com>
2025-12-26 18:12:06 +0200

    Add full Sanity config with plugins and product schema

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
 
+// Динамічний імпорт NextStudio
 const NextStudio = dynamic(() => import('next-sanity/studio').then(mod => mod.NextStudio), {
   ssr: false,
   loading: () => <div style={{
@@ -13,16 +17,43 @@ const NextStudio = dynamic(() => import('next-sanity/studio').then(mod => mod.Ne
   }}>Завантаження Sanity Studio...</div>
 })
 
-export default function StudioPage() {
-  const config = {
-    name: 'default',
-    title: 'Designer Bags UA Admin',
-    projectId: 'alskls9k',
-    dataset: 'production',
-    basePath: '/studio',
-    plugins: [],
-    schema: { types: [] },
-  }
+// Проста схема продукту
+const productSchema = {
+  name: 'product',
+  title: 'Товари / Products',
+  type: 'document',
+  fields: [
+    {
+      name: 'name_ua',
+      title: 'Назва (українською)',
+      type: 'string',
+    },
+    {
+      name: 'name_en',
+      title: 'Name (English)',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      title: 'Ціна / Price (грн)',
+      type: 'number',
+    },
+  ],
+}
 
+// Конфігурація Sanity
+const config = defineConfig({
+  name: 'default',
+  title: 'Designer Bags UA Admin',
+  projectId: 'alskls9k',
+  dataset: 'production',
+  basePath: '/studio',
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: [productSchema],
+  },
+})
+
+export default function StudioPage() {
   return <NextStudio config={config} />
 }
